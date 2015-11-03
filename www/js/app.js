e53ba866b77e99b674aeadcec5bd8e1b35da0ed3
Simon Wendel <mail@simonwendel.se>
2015-11-03 16:48:25 +0100

    Reformat app.js a bit more

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,25 @@
 ;(function () {
     'use strict';
 
-    angular.module('todo', ['ionic', 'todo.controllers', 'todo.services'])
+    angular
+        .module('todo', ['ionic', 'todo.controllers', 'todo.services'])
+        .run(setupIonic);
 
-        .run(function ($ionicPlatform) {
-            $ionicPlatform.ready(function () {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                    cordova.plugins.Keyboard.disableScroll(true);
+    /** @ngInject */
+    function setupIonic($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-                }
-                if (window.StatusBar) {
-                    // org.apache.cordova.statusbar required
-                    StatusBar.styleLightContent();
-                }
-            });
+            }
+
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
         });
+    }
 })();
