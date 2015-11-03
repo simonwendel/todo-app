20a5e0f28b7a806036a5b5bc8689299be1e3c466
Simon Wendel <mail@simonwendel.se>
2015-11-03 16:51:19 +0100

    Reformat routes.js abit more

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,26 +1,27 @@
 ;(function () {
     'use strict';
 
-    angular.module('todo')
-        .config(function($stateProvider, $urlRouterProvider) {
+    angular
+        .module('todo')
+        .config(setupAngularRoutes);
 
-            $stateProvider
-                .state('tab', {
-                    url: '/tab',
-                    abstract: true,
-                    templateUrl: 'templates/tabs.html'
-                })
-                .state('tab.dash', {
-                    url: '/dash',
-                    views: {
-                        'tab-dash': {
-                            templateUrl: 'templates/tab-todo.html',
-                            controller: 'DashCtrl'
-                        }
+    /** @ngInject */
+    function setupAngularRoutes($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            }).state('tab.dash', {
+                url: '/dash',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/tab-todo.html',
+                        controller: 'DashCtrl'
                     }
-                });
+                }
+            });
 
-            $urlRouterProvider.otherwise('/tab/dash');
-
-        });
+        $urlRouterProvider.otherwise('/tab/dash');
+    }
 })();
