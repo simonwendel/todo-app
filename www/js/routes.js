52db733f57b6358a1c0d450f96e6b872349dd508
Simon Wendel <mail@simonwendel.se>
2015-11-03 16:49:28 +0100

    Remove redundant comments from routes.js

--- www/js/routes.js
+++ www/js/routes.js
@@ -4,19 +4,12 @@
     angular.module('todo')
         .config(function($stateProvider, $urlRouterProvider) {
 
-            // Ionic uses AngularUI Router which uses the concept of states
-            // Learn more here: https://github.com/angular-ui/ui-router
-            // Set up the various states which the app can be in.
-            // Each state's controller can be found in controllers.js
             $stateProvider
-
-                // setup an abstract state for the tabs directive
                 .state('tab', {
                     url: '/tab',
                     abstract: true,
                     templateUrl: 'templates/tabs.html'
                 })
-
                 .state('tab.dash', {
                     url: '/dash',
                     views: {
@@ -27,7 +20,6 @@
                     }
                 });
 
-            // if none of the above states are matched, use this as the fallback
             $urlRouterProvider.otherwise('/tab/dash');
 
         });
