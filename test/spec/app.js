7e925da2e87e855193392bb5d6f421921809fed0
Simon Wendel <mail@simonwendel.se>
2015-11-04 11:31:33 +0100

    Cover todo module definition with test

---
+++ test/spec/app.js
@@ -0,0 +1,9 @@
+;(function () {
+    'use strict';
+
+    describe('Module: todo (app.js)', function () {
+        it('should be defined and accesible through angular module system.', function () {
+            expect(angular.module('todo')).toBeDefined();
+        });
+    });
+})();
