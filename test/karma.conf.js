b424942276e13582817087cfc99fe89a51ff89cb
Simon Wendel <mail@simonwendel.se>
2015-11-05 14:21:40 +0100

    Fix missing code coverage in karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'www/js/**/*.js': ['coverage']
+            'app/js/**/*.js': ['coverage']
         },
 
 
