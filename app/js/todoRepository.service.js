05d98a605d46bdb6a00e18d184941f09a4b7e972
Simon Wendel <mail@simonwendel.se>
2015-11-05 14:27:32 +0100

    Add some dummy repo data to todoRepository

--- app/js/todoRepository.service.js
+++ app/js/todoRepository.service.js
@@ -1,12 +1,78 @@
 ;(function () {
     'use strict';
 
+    var dummyTodos = [{
+            id: 1,
+            title: 'Something to do 1',
+            description: 'This is a longer description string to show in details',
+            created: '2015-11-05T14:09:39+01:00',
+            recurring: 5,
+            completed: [
+                '2015-11-05T14:09:39+01:00',
+                '2015-11-05T14:09:39+01:00'
+            ],
+            color: 'crimson'
+        },{
+            id: 10,
+            title: 'Something to do 2',
+            description: 'This is a longer description string to show in details',
+            created: '2015-11-05T14:09:39+01:00',
+            recurring: 5,
+            completed: [
+                '2015-11-05T14:09:39+01:00',
+                '2015-11-05T14:09:39+01:00'
+            ],
+            color: 'crimson'
+        },{
+            id: 11,
+            title: 'Something to do 3',
+            description: 'This is a longer description string to show in details',
+            created: '2015-11-05T14:09:39+01:00',
+            recurring: 5,
+            completed: [
+                '2015-11-05T14:09:39+01:00',
+                '2015-11-05T14:09:39+01:00'
+            ],
+            color: 'crimson'
+        },{
+            id: 21,
+            title: 'Something to do 4',
+            description: 'This is a longer description string to show in details',
+            created: '2015-11-05T14:09:39+01:00',
+            recurring: 5,
+            completed: [
+                '2015-11-05T14:09:39+01:00',
+                '2015-11-05T14:09:39+01:00'
+            ],
+            color: 'crimson'
+        }
+    ];
+
     angular
         .module('todo')
         .factory('todoRepository', todoRepository);
 
     /** @ngInject */
     function todoRepository() {
-        return {};
+        return {
+            getTodo: getTodo
+        };
+    }
+
+    function getTodo(id) {
+        if(id) {
+            var todos =
+                dummyTodos.filter(function filterDummyTodos(element) {
+                    return id === element.id;
+                });
+
+            if(todos.length !== 1) {
+                throw 'No such item found.';
+            }
+
+            return todos[0];
+        }
+
+        return dummyTodos;
     }
 })();
--- test/spec/todoRepository.service.js
+++ test/spec/todoRepository.service.js
@@ -12,6 +12,21 @@
         it('should be defined and accessible through the angular module system.', function () {
             expect(todoRepository).toBeDefined();
         });
+
+        it('should have a function for retrieving all todo items.', function () {
+            expect(todoRepository.getTodo().length).toBe(4);
+        });
+
+        it('should have a function for retrieving one todo item by id.', function () {
+            var todo = todoRepository.getTodo(11);
+            expect(todo.id).toBe(11);
+        });
+
+        it('should throw exception when no todo with specified id exists in the repository.', function () {
+            expect(function() {
+                todoRepository.getTodo(100);
+            }).toThrow('No such item found.');
+        });
     });
 
     function fixtureSetup(_todoRepository_) {
