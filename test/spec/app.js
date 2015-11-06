474ead7a08074b88d12df7d017ef8f6599c06ca3
Simon Wendel <mail@simonwendel.se>
2015-11-06 20:31:27 +0100

    Fix typo in app.js spec

--- test/spec/app.js
+++ test/spec/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     describe('Module: todo (app.js)', function () {
-        it('should be defined and accesible through angular module system.', function () {
+        it('should be defined and accessible through angular module system.', function () {
             expect(angular.module('todo')).toBeDefined();
         });
     });
