f598b29bbbcfd45e385f586ddaa35a1eb45e686f
Simon Wendel <mail@simonwendel.se>
2015-11-11 20:59:54 +0100

    Cleanup utilityFooter.directive.js

--- app/js/utilityFooter.directive.js
+++ app/js/utilityFooter.directive.js
@@ -5,18 +5,11 @@
         .module('todo')
         .directive('utilityFooter', utilityFooter);
 
-    /** @ngInject */
     function utilityFooter() {
         return {
             templateUrl: 'templates/utility-footer.html',
             restrict: 'E',
-            transclude: true,
-            scope: {},
-            controller: UtilityFooterController,
-            controllerAs: 'vm'
+            transclude: true
         };
-    }
-
-    function UtilityFooterController() {
     }
 })();
