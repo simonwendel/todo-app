1815d94ab5ed340eddc248c042c76c04fcf2156e
Simon Wendel <mail@simonwendel.se>
2015-11-18 20:58:39 +0100

    Add the build function to notificationService

--- app/js/notificationService.factory.js
+++ app/js/notificationService.factory.js
@@ -5,7 +5,26 @@
         .module('todo')
         .factory('notificationService', notificationService);
 
-    function notificationService() {
-        return {};
+    /** @ngInject */
+    function notificationService($rootScope) {
+        rootScope = $rootScope;
+
+        return {
+            build: build
+        };
+    }
+
+    var rootScope;
+
+    function build(eventName) {
+        return {
+            subscribe: function(scope, callback) {
+                var handler = rootScope.$on(eventName, callback);
+                scope.$on('$destroy', handler);
+            },
+            notify: function() {
+                rootScope.$emit(eventName);
+            }
+        };
     }
 })();
--- test/spec/notificationService.factory.js
+++ test/spec/notificationService.factory.js
@@ -1,7 +1,8 @@
 ;(function() {
     'use strict';
 
-    var notificationService;
+    var notificationService,
+        rootScope;
 
     describe('Factory: notificationService (notificationService.factory.js)', function() {
 
@@ -12,9 +13,43 @@
         it('should be defined and accessible.', function() {
             expect(notificationService).toBeDefined();
         });
+
+        it('should have a build function.', function() {
+            expect(notificationService.build('some event')).toBeDefined();
+        });
+
+        it('should return a notification service from build which consumers can subscribe to.', function() {
+            var someFunction = sinon.spy(),
+                scope = rootScope.$new(),
+                service = notificationService.build('some event');
+
+            service.subscribe(scope, someFunction);
+        });
+
+        it('should call subscriber back on notify.', function() {
+            var someFunction = sinon.spy(),
+                scope = rootScope.$new(),
+                service = notificationService.build('some event');
+
+            service.subscribe(scope, someFunction);
+            service.notify();
+            expect(someFunction.called).toBeTruthy();
+        });
+
+        it('should clean up on scope.$destroy.', function() {
+            var someFunction = sinon.spy(),
+                scope = rootScope.$new(),
+                service = notificationService.build('some event');
+
+            service.subscribe(scope, someFunction);
+            scope.$destroy();
+            service.notify();
+            expect(someFunction.called).toBeFalsy();
+        });
     });
 
-    function fixtureSetup(_notificationService_) {
+    function fixtureSetup(_notificationService_, $rootScope) {
         notificationService = _notificationService_;
+        rootScope = $rootScope;
     }
 })();
