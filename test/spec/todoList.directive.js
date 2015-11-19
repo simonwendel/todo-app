cd8600a222ad15dd77d96bc660d19e336203596a
Simon Wendel <mail@simonwendel.se>
2015-11-19 20:08:24 +0100

    Cover some todoList init code after-the-fact

--- test/spec/todoList.directive.js
+++ test/spec/todoList.directive.js
@@ -2,28 +2,39 @@
     'use strict';
 
     var element,
-        pageScope;
+        pageScope,
+        provide,
+        todoRepository;
 
     describe('Directive: todoList', function() {
 
         beforeEach(module('todo.test.templates'));
 
-        beforeEach(module('todo'));
+        beforeEach(module('todo', function fetchProvide($provide) {
+            provide = $provide;
+        }));
 
-        beforeEach(fixtureSetup);
+        beforeEach(inject(fixtureSetup));
 
         it('should be defined.', function() {
             expect(element.html()).not.toBe('');
         });
+
+        it('should call getTodo on todoRepository at init.', function() {
+            expect(todoRepository.getTodo.called).toBeTruthy();
+        });
     });
 
-    function fixtureSetup() {
-        inject(function($rootScope, $compile) {
-            pageScope = $rootScope.$new();
+    function fixtureSetup($rootScope, $compile) {
+        todoRepository = {
+            getTodo: sinon.spy()
+        };
 
-            element = angular.element('<todo-list></todo-list>');
-            element = $compile(element)(pageScope);
-            pageScope.$digest();
-        });
+        provide.value('todoRepository', todoRepository);
+        pageScope = $rootScope.$new();
+
+        element = angular.element('<todo-list></todo-list>');
+        element = $compile(element)(pageScope);
+        pageScope.$digest();
     }
 })();
