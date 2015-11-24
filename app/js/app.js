8fcc8dec6f24022d65949b8f5497ba47e613fdc6
Simon Wendel <mail@simonwendel.se>
2015-11-24 19:13:12 +0100

    Remove blank line in if-block

--- app/js/app.js
+++ app/js/app.js
@@ -13,7 +13,6 @@
             if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
                 cordova.plugins.Keyboard.disableScroll(true);
-
             }
 
             if (window.StatusBar) {
