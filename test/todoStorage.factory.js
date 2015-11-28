0697fd6c8443bffc8c8fc2bcfa53a6bc11fe43ba
Simon Wendel <mail@simonwendel.se>
2015-11-28 21:16:04 +0100

    Rework the todoStorage.factory.js tests for ES6

---
+++ test/todoStorage.factory.js
@@ -0,0 +1,21 @@
+import 'JamieMason/Jasmine-Matchers';
+import { todoStorage } from 'js/todoStorage.factory.js';
+
+describe('Factory: todoStorage (todoStorage.factory.js)', () => {
+
+    const storage = todoStorage();
+
+    it('should have an all() function to retrieve todos from storage.', () => {
+
+        expect(storage.all().length).toBe(4);
+
+    });
+
+    it('should have an save() function to save a todo to storage.', () => {
+
+        storage.save({});
+        expect(storage.all().length).toBe(5);
+
+    });
+
+});
