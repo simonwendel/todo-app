97e82b23b7e61e30393c6a993e3632f273535cf5
Simon Wendel <mail@simonwendel.se>
2015-11-29 19:02:49 +0100

    Wrap product of notificationServiceFactory in describe

--- test/notificationService.factory.js
+++ test/notificationService.factory.js
@@ -9,46 +9,50 @@ describe('Factory: notificationServiceFactory (notificationService.factory.js)',
 
     beforeEach(ng.inject(fixtureSetup));
 
-    it('should have a build function.', () => {
+    describe('Product: notificationService', () => {
 
-        expect(notificationService.build('some event')).toBeDefined();
+        it('should have a build function.', () => {
 
-    });
+            expect(notificationService.build('some event')).toBeDefined();
 
-    it('should return a notification service from build which consumers can subscribe to.', () => {
+        });
 
-        let someFunction = sinon.spy(),
-            scope = rootScope.$new(),
-            service = notificationService.build('some event');
+        it('should return a notification service from build which consumers can subscribe to.', () => {
 
-        service.subscribe(scope, someFunction);
+            let someFunction = sinon.spy(),
+                scope = rootScope.$new(),
+                service = notificationService.build('some event');
 
-    });
+            service.subscribe(scope, someFunction);
 
-    it('should call subscriber back on notify.', () => {
+        });
 
-        let someFunction = sinon.spy(),
-            scope = rootScope.$new(),
-            service = notificationService.build('some event');
+        it('should call subscriber back on notify.', () => {
 
-        service.subscribe(scope, someFunction);
-        service.notify();
-        expect(someFunction.called).toBeTruthy();
+            let someFunction = sinon.spy(),
+                scope = rootScope.$new(),
+                service = notificationService.build('some event');
 
-    });
+            service.subscribe(scope, someFunction);
+            service.notify();
+            expect(someFunction.called).toBeTruthy();
 
-    it('should clean up on scope.$destroy.', () => {
+        });
 
-        let someFunction = sinon.spy(),
-            scope = rootScope.$new(),
-            service = notificationService.build('some event');
+        it('should clean up on scope.$destroy.', () => {
 
-        service.subscribe(scope, someFunction);
-        scope.$destroy();
-        service.notify();
-        expect(someFunction.called).toBeFalsy();
+            let someFunction = sinon.spy(),
+                scope = rootScope.$new(),
+                service = notificationService.build('some event');
 
-    });
+            service.subscribe(scope, someFunction);
+            scope.$destroy();
+            service.notify();
+            expect(someFunction.called).toBeFalsy();
+
+        });
+
+    })
 
 });
 
@@ -56,4 +60,3 @@ function fixtureSetup($rootScope) {
     rootScope = $rootScope;
     notificationService = notificationServiceFactory(rootScope);
 }
-
