25e9db88a154a48c94aca187521292d57d407a76
Simon Wendel <mail@simonwendel.se>
2015-11-29 18:29:06 +0100

    Change anonymous fn into arrow fn in ionic config

--- js/ionic.config.js
+++ js/ionic.config.js
@@ -1,5 +1,5 @@
 function setupIonic($ionicPlatform) {
-    $ionicPlatform.ready(function() {
+    $ionicPlatform.ready(() => {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
