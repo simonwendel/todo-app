6369805b7bcc528e941622ee0e7301c6195fd2d7
Simon Wendel <mail@simonwendel.se>
2015-11-29 18:57:37 +0100

    Re-structure dateUtilityFactory tests

--- test/dateUtility.factory.js
+++ test/dateUtility.factory.js
@@ -5,21 +5,25 @@ describe('Factory: dateUtilityFactory (dateUtility.factory.js)', function() {
 
     const dateUtility = dateUtilityFactory();
 
-    it('should have an addDays function that can add a positive number of days to a date.', () => {
+    describe('Product dateUtility', () => {
 
-        var dec17 = new Date('December 17, 1995 03:24:00'),
-            jan2 = dateUtility.addDays(dec17, 16);
+        it('should have an addDays function that can add a positive number of days to a date.', () => {
 
-        expect(jan2).toEqual(new Date('January 2, 1996 03:24:00'));
+            let dec17 = new Date('December 17, 1995 03:24:00'),
+                jan2 = dateUtility.addDays(dec17, 16);
 
-    });
+            expect(jan2).toEqual(new Date('January 2, 1996 03:24:00'));
 
-    it('should have an addDays function that can add a negative number of days to a date.', () => {
+        });
 
-        var dec17 = new Date('December 17, 1995 03:24:00'),
-            nov28 = dateUtility.addDays(dec17, -19);
+        it('should have an addDays function that can add a negative number of days to a date.', () => {
 
-        expect(nov28).toEqual(new Date('November 28, 1995 03:24:00'));
+            let dec17 = new Date('December 17, 1995 03:24:00'),
+                nov28 = dateUtility.addDays(dec17, -19);
+
+            expect(nov28).toEqual(new Date('November 28, 1995 03:24:00'));
+
+        });
 
     });
 
