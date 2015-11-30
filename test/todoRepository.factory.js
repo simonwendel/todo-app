2e3af4e87bcb6b0a9a248930ea2482117989246e
Simon Wendel <mail@simonwendel.se>
2015-11-30 18:22:08 +0100

    Wrap product of todoRepositoryFactory in describe

--- test/todoRepository.factory.js
+++ test/todoRepository.factory.js
@@ -6,90 +6,88 @@ let todoRepository,
     notificationService,
     todoNotification;
 
-describe('Factory: todoRepository (todoRepository.factory.js)', () => {
+describe('Factory: todoRepositoryFactory (todoRepository.factory.js)', () => {
 
     beforeEach(fixtureSetup);
 
-    it('should be defined and accessible through the angular module system.', () => {
+    describe('Product: todoRepository', () => {
 
-        expect(todoRepository).toBeDefined();
+        it('should build a notification service with appropriate event name.', () => {
 
-    });
+            expect(notificationService.build.calledWith('todoRepositoryFactory.update')).toBeTruthy();
 
-    it('should build a notification service with appropriate event name.', () => {
+        });
 
-        expect(notificationService.build.calledWith('todoRepositoryFactory.update')).toBeTruthy();
+        it('should expose a subscriber interface using notificationService.', () => {
 
-    });
+            todoRepository.subscribe(() => {});
+            expect(todoNotification.subscribe.called).toBeTruthy();
 
-    it('should expose a subscriber interface using notificationService.', () => {
+        });
 
-        todoRepository.subscribe(() => {});
-        expect(todoNotification.subscribe.called).toBeTruthy();
+        it('should call the all() function of todoStorage to get todo items..', () => {
 
-    });
+            todoRepository.getTodo();
+            expect(todoStorage.all.called).toBeTruthy();
 
-    it('should call the all() function of todoStorage to get todo items..', () => {
+        });
 
-        todoRepository.getTodo();
-        expect(todoStorage.all.called).toBeTruthy();
+        it('should have a function for retrieving one todo item by id.', () => {
 
-    });
+            var todo = todoRepository.getTodo(11);
+            expect(todo.id).toBe(11);
 
-    it('should have a function for retrieving one todo item by id.', () => {
+        });
 
-        var todo = todoRepository.getTodo(11);
-        expect(todo.id).toBe(11);
+        it('should throw exception when no todo with specified id exists in the repository.', () => {
 
-    });
+            expect(() => {
+                todoRepository.getTodo(100);
+            }).toThrow();
 
-    it('should throw exception when no todo with specified id exists in the repository.', () => {
+        });
 
-        expect(() => {
-            todoRepository.getTodo(100);
-        }).toThrow();
+        it('should call the save() function on todoStorage to save a new todo item.', () => {
 
-    });
+            todoRepository.newTodo({});
+            expect(todoStorage.save.called).toBeTruthy();
 
-    it('should call the save() function on todoStorage to save a new todo item.', () => {
+        });
 
-        todoRepository.newTodo({});
-        expect(todoStorage.save.called).toBeTruthy();
+        it('should call the notify function notifying subscribers on newTodo.', () => {
 
-    });
+            todoRepository.newTodo({});
+            expect(todoNotification.notify.called).toBeTruthy();
 
-    it('should call the notify function notifying subscribers on newTodo.', () => {
+        });
 
-        todoRepository.newTodo({});
-        expect(todoNotification.notify.called).toBeTruthy();
+        it('should throw exception when null todo item is saved.', () => {
 
-    });
+            expect(() => {
+                todoRepository.newTodo(null);
+            }).toThrow();
 
-    it('should throw exception when null todo item is saved.', () => {
+        });
 
-        expect(() => {
-            todoRepository.newTodo(null);
-        }).toThrow();
+        it('should not notify subscribers if todo item to newTodo is null.', () => {
 
-    });
+            expect(() => {
+                todoRepository.newTodo(null);
+            }).toThrow();
+            expect(todoNotification.notify.called).toBeFalsy();
 
-    it('should not notify subscribers if todo item to newTodo is null.', () => {
+        });
 
-        expect(() => {
-            todoRepository.newTodo(null);
-        }).toThrow();
-        expect(todoNotification.notify.called).toBeFalsy();
+        it('should not save when null todo item is saved.', () => {
 
-    });
+            try {
+                todoRepository.newTodo(null);
+            } catch (e) {
+            }
 
-    it('should not save when null todo item is saved.', () => {
+            expect(todoStorage.save.called).toBeFalsy();
 
-        try {
-            todoRepository.newTodo(null);
-        } catch (e) {
-        }
-
-        expect(todoStorage.save.called).toBeFalsy();
+        });
 
     });
 });
