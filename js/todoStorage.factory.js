ce5399f2ab0d8d9c7a0c42e9ac66bea2f069f869
Simon Wendel <mail@simonwendel.se>
2015-11-30 19:12:42 +0100

    Insert some randomness in mock storage

--- js/todoStorage.factory.js
+++ js/todoStorage.factory.js
@@ -5,30 +5,27 @@ let staticTodos = [{
     created: '2015-11-05T14:09:39+01:00',
     recurring: 5,
     completed: [
-        '2015-11-05T14:09:39+01:00',
-        '2015-11-05T14:09:39+01:00'
+        '2015-11-15T14:09:39+01:00',
+        '2015-11-25T14:09:39+01:00'
     ],
     color: 'crimson'
 }, {
     id: 10,
     title: 'Something to do 2',
     description: 'This is a longer description string to show in details',
-    created: '2015-11-05T14:09:39+01:00',
-    recurring: 5,
+    created: '2015-11-21T14:09:39+01:00',
+    recurring: 15,
     completed: [
-        '2015-11-05T14:09:39+01:00',
-        '2015-11-05T14:09:39+01:00'
     ],
     color: 'seagreen'
 }, {
     id: 11,
     title: 'Something to do 3',
     description: 'This is a longer description string to show in details',
-    created: '2015-11-05T14:09:39+01:00',
-    recurring: 5,
+    created: '2015-11-22T14:09:39+01:00',
+    recurring: 7,
     completed: [
-        '2015-11-05T14:09:39+01:00',
-        '2015-11-05T14:09:39+01:00'
+        '2015-11-22T14:09:39+01:00',
     ],
     color: 'purple'
 }, {
@@ -36,10 +33,9 @@ let staticTodos = [{
     title: 'Something to do 4',
     description: 'This is a longer description string to show in details',
     created: '2015-11-05T14:09:39+01:00',
-    recurring: 5,
+    recurring: 10,
     completed: [
-        '2015-11-05T14:09:39+01:00',
-        '2015-11-05T14:09:39+01:00'
+        '2015-11-25T14:09:39+01:00'
     ],
     color: 'seagreen'
 }];
