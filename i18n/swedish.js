37063fa2b02f4915de117d49a4767dd9f84d8623
Simon Wendel <mail@simonwendel.se>
2015-12-01 19:53:06 +0100

    Add swedish i18n

---
+++ i18n/swedish.js
@@ -0,0 +1,11 @@
+const swedish = {
+   'ADD_TASK__UPPER': 'NY UPPGIFT',
+   'TITLE__PASCAL': 'Titel',
+   'DESCRIPTION__PASCAL': 'Beskrivning',
+   'COLOR__PASCAL': 'Färg',
+   'RECURRING_(DAYS)__PASCAL': 'Återkommande (dagar)',
+   'SAVE__UPPER': 'SPARA',
+   'CLOSE__UPPER': 'STÄNG'
+};
+
+export { swedish };
--- js/config/translations.js
+++ js/config/translations.js
@@ -1,7 +1,9 @@
 import { english } from 'i18n/english';
+import { swedish } from 'i18n/swedish';
 
 function translationSetup($translateProvider) {
     $translateProvider.translations('en', english);
+    $translateProvider.translations('sv', swedish);
     $translateProvider.preferredLanguage('en');
 }
 
