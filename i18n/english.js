5584d0cf5bf0c1ae56d5238629b8b245ed499162
Simon Wendel <mail@simonwendel.se>
2015-12-01 19:55:21 +0100

    Re-order translation keys

--- i18n/english.js
+++ i18n/english.js
@@ -1,11 +1,12 @@
 const english = {
     'ADD_TASK__UPPER': 'ADD TASK',
+    'SAVE__UPPER': 'SAVE',
+    'CLOSE__UPPER': 'CLOSE',
+
     'TITLE__PASCAL': 'Title',
     'DESCRIPTION__PASCAL': 'Description',
     'COLOR__PASCAL': 'Color',
-    'RECURRING_(DAYS)__PASCAL': 'Recurring (days)',
-    'SAVE__UPPER': 'SAVE',
-    'CLOSE__UPPER': 'CLOSE'
+    'RECURRING_(DAYS)__PASCAL': 'Recurring (days)'
 };
 
 export { english };
--- i18n/swedish.js
+++ i18n/swedish.js
@@ -1,11 +1,12 @@
 const swedish = {
    'ADD_TASK__UPPER': 'NY UPPGIFT',
+   'SAVE__UPPER': 'SPARA',
+   'CLOSE__UPPER': 'STÄNG',
+   
    'TITLE__PASCAL': 'Titel',
    'DESCRIPTION__PASCAL': 'Beskrivning',
    'COLOR__PASCAL': 'Färg',
-   'RECURRING_(DAYS)__PASCAL': 'Återkommande (dagar)',
-   'SAVE__UPPER': 'SPARA',
-   'CLOSE__UPPER': 'STÄNG'
+   'RECURRING_(DAYS)__PASCAL': 'Återkommande (dagar)'
 };
 
 export { swedish };
