5fe0cb142722a382f1b2b514ca1b785dc476a80a
Simon Wendel <mail@simonwendel.se>
2015-12-01 19:31:29 +0100

    Extract all text strings into english translation file

--- i18n/english.js
+++ i18n/english.js
@@ -1,3 +1,11 @@
-const english = {};
+const english = {
+    'ADD_TASK__UPPER': 'ADD TASK',
+    'TITLE__PASCAL': 'Title',
+    'DESCRIPTION__PASCAL': 'Description',
+    'COLOR__PASCAL': 'Color',
+    'RECURRING_(DAYS)__PASCAL': 'Recurring (days)',
+    'SAVE__UPPER': 'SAVE',
+    'CLOSE__UPPER': 'CLOSE'
+};
 
 export { english };
