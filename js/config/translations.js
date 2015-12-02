da10e47f38aecf694e8c2501506ca75116bca98f
Simon Wendel <mail@simonwendel.se>
2015-12-02 18:23:55 +0100

    Add sanitation strategy for angular-translate
    According to http://angular-translate.github.io/docs/#/guide/19_security
    the future default is 'sanitize' and that is chosen for now.

--- js/config/translations.js
+++ js/config/translations.js
@@ -2,6 +2,7 @@ import { english } from 'i18n/english';
 import { swedish } from 'i18n/swedish';
 
 function translationSetup($translateProvider) {
+    $translateProvider.useSanitizeValueStrategy('sanitize');
     $translateProvider.translations('en', english);
     $translateProvider.translations('sv', swedish);
     $translateProvider.preferredLanguage('en');
