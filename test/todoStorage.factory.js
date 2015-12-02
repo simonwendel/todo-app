6672255655259a4c518b182c5e8142af1e57c62e
Simon Wendel <mail@simonwendel.se>
2015-12-02 20:18:47 +0100

    Wrap it:s in product describe in todoStorage tests

--- test/todoStorage.factory.js
+++ test/todoStorage.factory.js
@@ -1,20 +1,24 @@
 import 'JamieMason/Jasmine-Matchers';
 import { todoStorageFactory } from 'js/todoStorage.factory';
 
+const todoStorage = todoStorageFactory();
+
 describe('Factory: todoStorageFactory (todoStorage.factory.js)', () => {
 
-    const todoStorage = todoStorageFactory();
+    describe('Product: todoStorage', () => {
 
-    it('should have an all() function to retrieve todos from storage.', () => {
+        it('should have an all() function to retrieve todos from storage.', () => {
 
-        expect(todoStorage.all().length).toBe(4);
+            expect(todoStorage.all().length).toBe(4);
 
-    });
+        });
 
-    it('should have an save() function to save a todo to storage.', () => {
+        it('should have an save() function to save a todo to storage.', () => {
 
-        todoStorage.save({});
-        expect(todoStorage.all().length).toBe(5);
+            todoStorage.save({});
+            expect(todoStorage.all().length).toBe(5);
+
+        });
 
     });
 
