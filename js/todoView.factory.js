b62b35912e131a1093e34106fb7bc1b8a0db53a5
Simon Wendel <mail@simonwendel.se>
2015-12-04 21:43:27 +0100

    Add getDate fn to todoView

--- js/todoView.factory.js
+++ js/todoView.factory.js
@@ -1,5 +1,23 @@
-function todoViewFactory() {
-    return {};
+let dateUtilityService,
+    selectedDate;
+
+todoViewFactory.$inject = ['dateUtility'];
+function todoViewFactory(dateUtility) {
+    dateUtilityService = dateUtility;
+    selectedDate = now();
+
+    return {
+        getDate: getDate
+    };
+}
+
+function now() {
+    return dateUtilityService.now();
+}
+
+function getDate() {
+    return dateUtilityService
+        .display(selectedDate);
 }
 
 export { todoViewFactory };
--- test/todoView.factory.js
+++ test/todoView.factory.js
@@ -1,9 +1,14 @@
 import 'JamieMason/Jasmine-Matchers';
 import { todoViewFactory } from 'js/todoView.factory';
 
-const todoView = todoViewFactory();
+const rms = '1953-03-16';
+
+let todoView,
+    dateUtilityMock;
 
 describe('Factory: todoViewFactory (todoView.factory.js)', () => {
+
+    beforeEach(fixtureSetup);
 
     describe('Product: todoView', () => {
 
@@ -13,6 +18,24 @@ describe('Factory: todoViewFactory (todoView.factory.js)', () => {
 
         });
 
+        it('should have a function to get the view date as a string.', () => {
+
+            let date = todoView.getDate();
+            expect(dateUtilityMock.now.called).toBe(true);
+            expect(dateUtilityMock.display.called).toBe(true);
+            expect(date).toBe(rms);
+
+        });
+
     });
 
 });
+
+function fixtureSetup() {
+    dateUtilityMock = {
+        now: sinon.stub(),
+        display: sinon.stub().returns(rms)
+    };
+
+    todoView = todoViewFactory(dateUtilityMock);
+}
