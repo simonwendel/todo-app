6bd97bcc1ce3f48f83d407ab73230b9bf4f15612
Simon Wendel <mail@simonwendel.se>
2015-12-20 19:50:14 +0100

    Rename todo and alter dates in static storage

--- js/storage.factory.js
+++ js/storage.factory.js
@@ -4,7 +4,7 @@ import { Todo, Color } from 'js/types';
 let staticTodos = [
     new Todo({
         id: 1,
-        title: 'Something to do 1',
+        title: 'Something to do 2015-12-22',
         description: 'This is a longer description string to show in details',
         created: new Date('2015-11-05T14:09:39+01:00'),
         recurring: 5,
@@ -12,7 +12,7 @@ let staticTodos = [
         nextOccurrance: new Date('2015-12-22T00:00:00+01:00')
     }), new Todo({
         id: 10,
-        title: 'Something to do 2',
+        title: 'Something to do 2015-12-22',
         description: 'This is a longer description string to show in details',
         created: new Date('2015-11-21T14:09:39+01:00'),
         recurring: 15,
@@ -20,20 +20,20 @@ let staticTodos = [
         nextOccurrance: new Date('2015-12-22T00:00:00+01:00')
     }), new Todo({
         id: 11,
-        title: 'Something to do 3',
+        title: 'Something to do 2015-11-22',
         description: 'This is a longer description string to show in details',
         created: new Date('2015-11-22T14:09:39+01:00'),
         recurring: 7,
         color: new Color('PURPLE__COLOR', 'purple'),
-        nextOccurrance: new Date('2015-12-22T00:00:00+01:00')
+        nextOccurrance: new Date('2015-11-22T00:00:00+01:00')
     }), new Todo({
         id: 21,
-        title: 'Something to do 4',
+        title: 'Something to do 2015-12-02',
         description: 'This is a longer description string to show in details',
         created: new Date('2015-11-05T14:09:39+01:00'),
         recurring: 10,
         color: new Color('SEAGREEN__COLOR', 'seagreen'),
-        nextOccurrance: new Date('2015-12-22T00:00:00+01:00')
+        nextOccurrance: new Date('2015-12-02T00:00:00+01:00')
     })
 ];
 
