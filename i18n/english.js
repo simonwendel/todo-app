8868158704f899d1be915940aaf5976db89e7e9a
Simon Wendel <mail@simonwendel.se>
2015-12-27 14:47:49 +0100

    Add HTML for the delete modal

--- i18n/english.js
+++ i18n/english.js
@@ -1,16 +1,21 @@
 const english = {
     'ADD_TASK__TRANSLATION__UPPER': 'ADD TASK',
-    'SAVE__TRANSLATION__UPPER': 'SAVE',
     'CLOSE__TRANSLATION__UPPER': 'CLOSE',
 
     // todo list
     'NOTHING_TO_DO__TRANSLATION': 'Nothing to do right now...',
 
     // new todo modal
+    'SAVE__TRANSLATION__UPPER': 'SAVE',
     'TITLE__TRANSLATION': 'Title',
     'DESCRIPTION__TRANSLATION': 'Description',
     'COLOR__TRANSLATION': 'Color',
     'RECURRING_(DAYS)__TRANSLATION': 'Recurring (days)',
+
+    // delete todo modal
+    'DELETE_TASK__TRANSLATION__UPPER': 'DELETE TASK',
+    'DELETE__TRANSLATION__UPPER': 'DELETE',
+    'ARE_YOU_SURE__TRANSLATION': 'Are you sure you want to delete this item?',
 
     // colors
     'CRIMSON__COLOR': 'Crimson',
--- i18n/swedish.js
+++ i18n/swedish.js
@@ -1,16 +1,21 @@
 const swedish = {
     'ADD_TASK__TRANSLATION__UPPER': 'NY UPPGIFT',
-    'SAVE__TRANSLATION__UPPER': 'SPARA',
     'CLOSE__TRANSLATION__UPPER': 'STÄNG',
 
     // todo list
     'NOTHING_TO_DO__TRANSLATION': 'Inget att göra just nu...',
 
     // new todo modal
+    'SAVE__TRANSLATION__UPPER': 'SPARA',
     'TITLE__TRANSLATION': 'Titel',
     'DESCRIPTION__TRANSLATION': 'Beskrivning',
     'COLOR__TRANSLATION': 'Färg',
     'RECURRING_(DAYS)__TRANSLATION': 'Återkommande (dagar)',
+
+    // delete todo modal
+    'DELETE_TASK__TRANSLATION__UPPER': 'TA BORT UPPGIFT',
+    'DELETE__TRANSLATION__UPPER': 'TA BORT',
+    'ARE_YOU_SURE__TRANSLATION': 'Är du säker på att du vill ta bort denna uppgift?',
 
     // colors
     'CRIMSON__COLOR': 'Röd',
