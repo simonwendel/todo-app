1372764a5b81ef81bbdca6a1d089b3877b9b9cc7
Simon Wendel <mail@simonwendel.se>
2016-01-02 13:51:02 +0100

    Change swedish characters into HTLM entities in translations

--- i18n/swedish.js
+++ i18n/swedish.js
@@ -1,32 +1,32 @@
 const swedish = {
     'ADD_TASK__TRANSLATION__UPPER': 'NY UPPGIFT',
-    'CLOSE__TRANSLATION__UPPER': 'STÄNG',
+    'CLOSE__TRANSLATION__UPPER': 'ST&Auml;NG',
 
     // todo list
-    'NOTHING_TO_DO__TRANSLATION': 'Inget att göra just nu...',
+    'NOTHING_TO_DO__TRANSLATION': 'Inget att g&ouml;ra just nu...',
 
     // new todo modal
     'SAVE__TRANSLATION__UPPER': 'SPARA',
     'TITLE__TRANSLATION': 'Titel',
     'DESCRIPTION__TRANSLATION': 'Beskrivning',
-    'COLOR__TRANSLATION': 'Färg',
-    'RECURRING_(DAYS)__TRANSLATION': 'Återkommande (dagar)',
+    'COLOR__TRANSLATION': 'F&auml;rg',
+    'RECURRING_(DAYS)__TRANSLATION': '&Aring;terkommande (dagar)',
 
     // delete todo modal
     'DELETE_TASK__TRANSLATION__UPPER': 'TA BORT UPPGIFT',
     'DELETE__TRANSLATION__UPPER': 'TA BORT',
-    'ARE_YOU_SURE__TRANSLATION': 'Är du säker på att du vill ta bort denna uppgift?',
+    'ARE_YOU_SURE__TRANSLATION': '&Auml;r du s&auml;ker p&aring; att du vill ta bort denna uppgift?',
 
     // mark as done modal
-    'MARK_AS_DONE__TRANSLATION__UPPER': 'MARKERA SOM UTFÖRD',
+    'MARK_AS_DONE__TRANSLATION__UPPER': 'MARKERA SOM UTF&Ouml;RD',
     'MARK__TRANSLATION__UPPER': 'MARKERA',
-    'DO_YOU_WANT_TO_MARK__TRANSLATION': 'Vill du markera denna uppgift som utförd?',
+    'DO_YOU_WANT_TO_MARK__TRANSLATION': 'Vill du markera denna uppgift som utf&ouml;rd?',
 
     // colors
-    'CRIMSON__COLOR': 'Röd',
-    'DARKBLUE__COLOR': 'Mörkblå',
+    'CRIMSON__COLOR': 'R&ouml;d',
+    'DARKBLUE__COLOR': 'M&ouml;rkbl&aring;',
     'PURPLE__COLOR': 'Lila',
-    'SEAGREEN__COLOR': 'Grön',
+    'SEAGREEN__COLOR': 'Gr&ouml;n',
     'TOMATO__COLOR': 'Orange',
     'VIOLET__COLOR': 'Violett'
 };
